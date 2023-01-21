94b5a26371f6692b6fcf4aaa219c3d39cd66443e
Elena Rughi <elenarughi@gmail.com>
2023-01-21 14:37:31 +0100

    feat: add interfaces for shopping process services

---
+++ src/main/scala/shopping/shoppingprocess/services/CustomerStateHandlers.scala
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2022-2023 by Pervasive Cats S.r.l.s.
+ *
+ * All Rights Reserved.
+ */
+
+package io.github.pervasivecats
+package shopping.shoppingprocess.services
+
+import shopping.shoppingprocess.events.CustomerUnregistered
+
+trait CustomerStateHandlers {
+
+  def onCustomerUnregistered(event: CustomerUnregistered): Unit
+}
---
+++ src/main/scala/shopping/shoppingprocess/services/ShoppingProcessHandlers.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2022-2023 by Pervasive Cats S.r.l.s.
+ *
+ * All Rights Reserved.
+ */
+
+package io.github.pervasivecats
+package shopping.shoppingprocess.services
+
+import shopping.shoppingprocess.events.*
+
+trait ShoppingProcessHandlers {
+
+  def onCustomerHasEnteredStore(event: CustomerHasEnteredStore): Unit
+
+  def onCartAssociated(event: CartAssociated): Unit
+
+  def onCustomerHasLeftStore(event: CustomerHasLeftStore): Unit
+
+  def onTimeoutWasReached(event: TimeoutWasReached): Unit
+
+  def onCustomerHasPaid(event: CustomerHasPaid): Unit
+
+  def onItemAddedToCart(event: ItemAddedToCart): Unit
+
+  def onItemReturned(event: ItemReturned): Unit
+}
